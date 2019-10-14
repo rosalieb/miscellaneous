8483ce172d632906c72b4e912cad3b1c23bc6abd
Rosalie Bruel <Rosalie.Bruel@uvm.edu>
2019-10-14 17:45:56 -0400

    small cleaning edit

--- R/lessismore.R
+++ R/lessismore.R
@@ -13,24 +13,19 @@
 
 # Works for .R and .Rmd. If input is .Rmd, the function will first extract the code from chunk (creating a copy of the code in the working directory)
 
-packages <- c("base","mgcv","ggplot2", "FSA")
-# path2file<-"/Volumes/-/Script R/LEM10_CC/scriptRosalie060317.R"
-path2file<-"/Volumes/-/Script R/LEM10_CC/LEM10_CC_20190416.Rmd"
-# tmp<- getParseData(parse(path2file, keep.source = TRUE))
-# View(tmp)
 
 lessismore <- function(packages = NULL, path2file = NULL, plot_output = FALSE, thresh = 2) {
   if (!file.exists(path2file)) {
     stop("couldn't find file ", path2file)
   }
-  
+
   packages <- sort(packages)
-  
+
   require("NCmisc")
   require("sos")
   list_out <- list()
-  
-  
+
+
   # Step 0: If .Rmd file, need to extract the code in R chunks first
   # Use code in https://felixfan.github.io/extract-r-code/
   if(get.ext(path2file)=="Rmd") {
@@ -38,10 +33,10 @@ lessismore <- function(packages = NULL, path2file = NULL, plot_output = FALSE, t
     tmp <- purl(path2file)
     path2file <- paste(getwd(),tmp,sep="/")
   }
-  
+
   # Step 1, screen the script
   summ_script <- getParseData(parse(path2file, keep.source = TRUE))
-  
+
   # Step 2, extract the function
   list_fun <- summ_script$text[which(summ_script$token == "SYMBOL_FUNCTION_CALL")]
   # just for fun, get some summary stats:
@@ -52,18 +47,18 @@ lessismore <- function(packages = NULL, path2file = NULL, plot_output = FALSE, t
     require("plotly")
     summ_fun$name <- rownames(summ_fun)
     summ_fun <- summ_fun[order(summ_fun$count, decreasing = T),]
-    p <- ggplot(summ_fun, aes(index(summ_fun),count, label=summ_fun$name)) + geom_point() + geom_line() + 
+    p <- ggplot(summ_fun, aes(index(summ_fun),count, label=summ_fun$name)) + geom_point() + geom_line() +
       theme_bw() + labs(x="Index", y="Count", title="Occurence of each function in the script \n(Hoover to see name of the functions)")
     list_out$summary_functions_plot <- ggplotly(p)
   }
-  
+
   list_fun <- unique(list_fun)
   src <- paste(as.vector(sapply(list_fun, find)))
   src <- tapply(list_fun, factor(src), c)
   list_pack1 <- names(src)
-  
+
   if("character(0)" %in% list_pack1) vec_fun <- src$`character(0)` else vec_fun <- NULL
-  
+
   # If some functions were not matched, try to use the "sos" package that searches more widely
   options(warn=-1)
   list_pack2 <- NULL
@@ -73,7 +68,7 @@ lessismore <- function(packages = NULL, path2file = NULL, plot_output = FALSE, t
     output_sos <- findFn(vec_fun[i])
     output_sos <- output_sos[output_sos$Count>thresh,]
     if("FSA" %in% output_sos$Package) stop()
-    
+
     if(length(output_sos$Package>0)) {
       list_pack2 <- c(list_pack2,unique(output_sos$Package))
     } else {
@@ -82,20 +77,20 @@ lessismore <- function(packages = NULL, path2file = NULL, plot_output = FALSE, t
     }
     }
   options(warn=0)
-  
+
   # Find which packages are probably used at least once
   which_useful <- rep(FALSE, length(packages))
   for (i in 1:length(packages)) {
     if(length(grep(packages[i], c(list_pack1,list_pack2)))>0) which_useful[i] <- TRUE
   }
-  
+
   # Outputs
   list_out$packages_used <- packages[which_useful]
   list_out$packages_non_used <- packages[!which_useful]
   list_out$functions_non_matched <- no_match
-  
+
   cat(paste0("Out of the ",length(packages)," packages in the input vector:\n   ✓ ",length(list_out$packages_used)," are used within the script,\n   ✕ ",length(list_out$packages_non_used)," do not appear to be used within the script,\n",length(list_out$functions_non_matched)," functions are used within the script but do not seem to be part of any package or the Global Environment."))
   return(list_out)
-  
+
 }
 
